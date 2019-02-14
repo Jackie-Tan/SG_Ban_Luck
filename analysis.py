c326d331d6a1dace1871ebd0de7e60bcae589f52
Jack T <jwtan2010@gmail.com>
2019-02-14 23:31:10 +0800

    Add 'fixed' var, skew, kurtosis. Remove mode.
    - Add 'fixed' variable to control if the experiment terminates upon the first pair of aces or to run for a fixed number of shuffles before termination. The default fixed number is 1,000.
    - Display skewness, kurtosis and its test on the graph
    - Remove mode display on the graph

--- analysis.py
+++ analysis.py
@@ -7,18 +7,20 @@ from scipy import stats
 
 total_list = []
 for i in range(1000):
-	total_list.append(cp.run())
+	total_list.append(cp.run(fixed=True))
 mean = np.mean(total_list)
 median = np.median(total_list)
-mode = stats.mode(total_list)[0]
+skew = stats.skew(total_list)
+kurtosis = stats.kurtosis(total_list)
+kurtosis_test = stats.kurtosistest(total_list)
 std = np.std(total_list)
 total_time = time.time() - start_time
 print(f"The program took {total_time}s to run.")
 
-plt.hist(total_list, bins=50, range=(0,0.05), density=True, weights=None, cumulative=False, bottom=None, histtype='bar', align='mid', orientation='vertical', rwidth=None, log=False, color="Black", label=None, stacked=False, data=None)
+plt.hist(total_list, density=False, weights=None, cumulative=False, bottom=None, histtype='bar', align='mid', orientation='vertical', rwidth=None, log=False, color="Black", label=None, stacked=False, data=None)
 plt.xlabel("Probabilities")
 plt.ylabel("Occurrences")
 plt.title("Ban Ban Analysis")
-plt.text(0.03, 100, f"Mean: {mean}\nMedian: {median}\nMode: {mode}\nStd. Dev.: {std}")
+plt.text(0.01, 100, f"Mean: {mean}\nMedian: {median}\nStd. Dev.: {std}\nSkew: {skew}\nKurtosis: {kurtosis}\n{kurtosis_test}")
 plt.grid(True)
 plt.show()
--- check_prob.py
+++ check_prob.py
@@ -2,17 +2,21 @@ import random
 from deck import Deck
 
 game_deck = Deck()
-def run():
+def run(fixed=False):
 	counter = 1
+	num_aces_pair = 0
+	stop_limit = 1000
 	while True:
 		# Shuffle deck
 		random.shuffle(game_deck.deck)
 		# Take 2 cards from the top
 		hand = game_deck.deck[0:2]
-		# print(f"{counter}: {hand}")
 		# Check if both cards are Aces, 
 		if hand[0][0] == 'A' and hand[1][0] == 'A':
-			#print("BAN BAN!")
-			#print(1/counter)
-			return 1/counter	
+			if fixed == False:
+				return 1/counter
+			else:
+				num_aces_pair += 1
 		counter += 1
+		if counter > stop_limit:
+			return num_aces_pair/counter
